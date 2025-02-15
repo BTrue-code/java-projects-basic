b6fb0e8c51908badfa256d970c3c41114558f330
BTrue-code <b.true1990@gmail.com>
2025-02-15 17:14:50 -0500

    GitHub commit update #4

--- src/main/java/org/example/CodingTaskE7.java
+++ src/main/java/org/example/CodingTaskE7.java
@@ -5,7 +5,7 @@ public class CodingTaskE7 {
     public static void main(String[] args) {
 
     //Write a Java Program to print the first 10 numbers of Fibonacci series
-    int limit = 10, firstNum = 0, secondNum = 1;
+    int limit = 20, firstNum = 0, secondNum = 1;
     int nextNum;
 
     // for loop example
